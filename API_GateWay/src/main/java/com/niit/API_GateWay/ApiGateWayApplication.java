09015c898756641288962d7690e79537becd9b3e
USER <rajputshubham615@gmail.com>
2023-01-07 11:48:33 +0530

    added the EurekaServerApplication and ApiGateWayApplication in the Delivery_CUbe application

--- API_GateWay/src/main/java/com/niit/API_GateWay/ApiGateWayApplication.java
+++ API_GateWay/src/main/java/com/niit/API_GateWay/ApiGateWayApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGateWayApplication {
 
 	public static void main(String[] args) {
--- FavoriteService/src/main/java/com/niit/FavoriteServiceApplication.java
+++ FavoriteService/src/main/java/com/niit/FavoriteServiceApplication.java
@@ -2,8 +2,10 @@ package com.niit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FavoriteServiceApplication {
 
 	public static void main(String[] args) {

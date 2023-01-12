c3a94cea5adbc406d1aa75805e8174f8f9df807c
USER <rajputshubham615@gmail.com>
2023-01-12 10:20:55 +0530

    added the consumer for order and cart  in the FavoriteServices

--- FavoriteService/src/main/java/com/niit/configuration/Consumer.java
+++ FavoriteService/src/main/java/com/niit/configuration/Consumer.java
@@ -1,16 +1,14 @@
 package com.niit.configuration;
 
 
+import com.niit.domain.Cart;
 import com.niit.domain.Favorite;
+import com.niit.domain.Order;
 import com.niit.service.FavoriteServiceImpl;
-import com.niit.service.IFavoriteService;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class Consumer {
@@ -25,8 +23,15 @@ public class Consumer {
 	@RabbitListener(queuesToDeclare = @Queue("messageQueue"))
 	public void getData(MessageDTO messageDTO) {
 		Favorite favorite = new Favorite();
+		Cart cart = new Cart();
+		Order order = new Order();
+		order.setEmail(messageDTO.getEmail());
+		cart.setEmail(messageDTO.getEmail());
 		favorite.setEmail(messageDTO.getEmail());
 		System.out.println(favorite);
 		favoriteServiceimpl.saveFavorite(favorite);
+		favoriteServiceimpl.saveCart(cart);
+		favoriteServiceimpl.saveOrder(order);
+
 	}
 }
